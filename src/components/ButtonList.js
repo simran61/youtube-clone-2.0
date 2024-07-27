c0c8ea818fab4a0243ae11e6ad63f5734b636c66
simran61 <simrangangwani3107@gmail.com>
2024-07-27 22:18:50 +0530

    update: created buttonList comp

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex m-2">
+      <Button name="All" />
+      <Button name="Music" />
+      <Button name="Live" />
+      <Button name="Javascript" />
+      <Button name="Information Technology" />
+      <Button name="MacBooks" />
+      <Button name="Smartphones" />
+      <Button name="Comedy" />
+    </div>
+  );
 };
 
 export default ButtonList;
