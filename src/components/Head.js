dbe37afbb8b8f456540e87728a0b16a0561a5dc0
simran61 <simrangangwani3107@gmail.com>
2024-07-27 18:52:07 +0530

    style: styled head comp

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,7 +1,61 @@
 import React from "react";
 
 const Head = () => {
-  return <div>Head</div>;
+  return (
+    <div className="flex justify-between items-center p-5 shadow-lg">
+      <div className="flex">
+        <img
+          className="h-8 cursor-pointer"
+          alt="menu"
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
+        />
+        <img
+          className="h-7 mx-4"
+          alt="youtube-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+        />
+      </div>
+      <div className="px-10 flex items-center">
+        <div className="flex">
+          <input
+            className="px-5 w-96 border border-gray-400 p-2 rounded-l-full"
+            type="text"
+          />
+          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+            <img
+              className="h-4"
+              src="https://static-00.iconduck.com/assets.00/search-icon-2048x2048-cmujl7en.png"
+              alt="search"
+            />
+          </button>
+        </div>
+        <div className="mx-4 bg-gray-200 h-11 w-11 flex items-center justify-center rounded-full">
+          <img
+            className="h-5"
+            src="https://static-00.iconduck.com/assets.00/microphone-icon-820x1024-sm9b2ks9.png"
+            alt="voice"
+          />
+        </div>
+      </div>
+      <div className="flex items-center">
+        <img
+          className="h-8"
+          alt="video-camera"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Video_camera_icon.svg/1200px-Video_camera_icon.svg.png"
+        />
+        <img
+          className="h-7 mx-3"
+          alt="notification"
+          src="https://cdn-icons-png.flaticon.com/512/3119/3119338.png"
+        />
+        <img
+          className="h-8"
+          alt="user"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Head;
