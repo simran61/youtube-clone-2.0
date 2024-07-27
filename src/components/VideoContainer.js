a4106324e86cdbced2150321a54292a869a24c97
simran61 <simrangangwani3107@gmail.com>
2024-07-28 00:40:00 +0530

    made API call

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
 
 const VideoContainer = () => {
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    console.log(json);
+  };
+
   return <div>VideoContainer</div>;
 };
 
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyAkAub7dRchC3D5yIJpTEGxwlqOQ-U9pKk";
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  GOOGLE_API_KEY;
