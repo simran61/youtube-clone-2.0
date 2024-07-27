acb03159cc0e7515fc4b51ab929e8e0f6f768ce6
simran61 <simrangangwani3107@gmail.com>
2024-07-27 21:59:04 +0530

    feat: hamburger toggle using RTK

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  if (!isMenuOpen) return null;
+
   return (
     <div className="p-5 border-r-2 w-52 h-[87.5vh]">
       <ul>
