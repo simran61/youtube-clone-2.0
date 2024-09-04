9363b31f23130b0baede19cfd2c75cbddbdd1d1e
simran61 <simrangangwani3107@gmail.com>
2024-09-04 16:08:37 +0530

    refactor: ad components

--- src/components/AdContainer.js
+++ src/components/AdContainer.js
@@ -1,43 +1,41 @@
 import React from "react";
+import adBanner from "../assets/images/adBanner.jpg";
+import adChannel from "../assets/images/adChannel.jpg";
+import DotsMenu from "../assets/icons/DotsMenu";
+import { Link } from "react-router-dom";
 
 const AdContainer = () => {
   return (
-    <div className="border rounded-xl w-[402px] h-[139px] dark:border-zinc-700">
-      <img
-        className="rounded-t-xl"
-        src="https://yt3.ggpht.com/8GMdBOhDKTRWycnl1PKXWPq1BO9lQG-FMv8w2TIqEmAA9smpad9VbISrldyUWO08X3b0J-wr9w=w1060-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
-        alt="adImg"
-      />
-      <div className="flex justify-between items-center ml-4 my-3">
-        <div className="flex items-center">
-          <img
-            className="w-9 h-9 rounded-full"
-            src="https://yt3.ggpht.com/mNNzuyntxCDSt_sd6j4bGAwfNbrH-neUe4HlSw4fsK7pGWMr7r0R8_HjEPR17Cufi6AaPggr=s88-c-k-c0x00ffffff-no-rj"
-            alt="adChannel"
-          />
-          <div className="mx-4 dark:text-white">
-            <h3>NIVEA India</h3>
-            <div className="text-xs">
-              <span className="font-bold">Sponsered</span>
-              <span className="text-[#606060] dark:text-[#aaaaaa]">
-                {" "}
-                • www.nive.in
-              </span>
+    <Link to="https://www.nivea.in/">
+      <div className="border rounded-xl w-[402px] h-[139px] dark:border-zinc-700">
+        <img className="rounded-t-xl" src={adBanner} alt="adImg" />
+        <div className="flex justify-between items-center ml-4 my-3">
+          <div className="flex items-center">
+            <img
+              className="w-9 h-9 rounded-full"
+              src={adChannel}
+              alt="adChannel"
+            />
+            <div className="mx-4 dark:text-white">
+              <h3>NIVEA India</h3>
+              <div className="text-xs">
+                <span className="font-bold">Sponsered</span>
+                <span className="text-[#606060] dark:text-[#aaaaaa]">
+                  {" "}
+                  • www.nivea.in
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        <button className="bg-blue-600 px-4 py-2 rounded-3xl font-semibold text-white">
-          Know more
-        </button>
-        <div className="flex items-center justify-between h-9 w-9 mr-1">
-          <img
-            className="h-5"
-            src="https://www.svgrepo.com/show/345223/three-dots-vertical.svg"
-            alt=""
-          />
+          <button className="bg-blue-600 px-4 py-2 rounded-3xl font-semibold text-white">
+            Know more
+          </button>
+          <div className="flex items-center justify-between h-9 w-9 mr-1">
+            <DotsMenu />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/AdVideo.js
+++ src/components/AdVideo.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import DotsMenu from "../assets/icons/DotsMenu";
+import adVideoChannel from "../assets/images/adVideoChannel.jpg";
+import adVideoBanner from "../assets/images/adVideoBanner.webp";
 
 const AdVideo = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
@@ -10,15 +13,15 @@ const AdVideo = () => {
       <div className={`mx-2 my-6 ${isMenuOpen ? "w-[296px]" : "w-[267px]"}`}>
         <img
           className="rounded-xl hover:rounded-none"
-          src="https://i.ytimg.com/vi/QVNpTiFsGvw/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAZfV6zBC_usOgeX4DTeNyfM56fJA"
-          alt=""
+          src={adVideoBanner}
+          alt="thumbnail"
         />
         <div className="flex justify-between my-2">
           <div className="flex items-center">
             <img
               className="h-9 mr-2 rounded-full"
-              alt="user"
-              src="https://yt3.ggpht.com/ioD47o1fxrL6SaynAejO20wJ4o_8xQn8K4cWvnyCpahR3X8IifAFdVm8xsEN4oODRddMonkagw=s88-c-k-c0x00ffffff-no-rj"
+              alt="channel"
+              src={adVideoChannel}
             />
             <div>
               <h2 className="font-semibold text-ellipsis overflow-hidden line-clamp-2 dark:text-white">
@@ -34,17 +37,7 @@ const AdVideo = () => {
               </div>
             </div>
           </div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            enable-background="new 0 0 24 24"
-            height="24"
-            viewBox="0 0 24 24"
-            width="24"
-            focusable="false"
-            aria-hidden="true"
-          >
-            <path d="M12 16.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5zM10.5 12c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5zm0-6c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5z"></path>
-          </svg>
+          <DotsMenu />
         </div>
         <div className="flex justify-between py-1">
           <button className="w-[142px] p-2 rounded-3xl border text-[#3EA6FF] font-medium text-sm dark:border-zinc-700">
